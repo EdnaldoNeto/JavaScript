b47b04f6ebe4497edefb10c1a3d99b71fb4bc600
Ednaldo_Neto <ednaldoramosneto@gmail.com>
2024-08-27 00:02:56 -0300

    create .js

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var speed = 75;
+
+console.log(`A velocidade do seu automóvel está em: ${speed}Km/h`);
+
+if(speed > 70) //condição simples
+{
+    console.log('A sua velocidade está acima da permitida. VOCÊ FOI MULTADO!');
+}
+
+console.log('Dirija com cuidado');
