b63b5b38974a3b1c8b949eb84bb57a3843220fc3
Ednaldo_Neto <ednaldoramosneto@gmail.com>
2024-09-05 21:34:45 -0300

    upade.list

--- exe/to-do-list.js
+++ exe/to-do-list.js
@@ -1,1 +1,23 @@
-var button = window.document.getElementById('butt')
+var button = document.getElementById('butt')
+var novaTarefa = document.getElementById('res')
+var verificar1 = document.getElementById('tarefa')
+var paragrafo = document.createElement('p')
+var quebra_linha = document.createElement('br')
+
+function add()
+{
+    if(verificar1.value.length == 0)
+    {
+        alert("Digite a sua nova tarefa")
+    } else {
+        while (verificar1.value.length != 0)
+        {
+            paragrafo.append(verificar1.value)
+            
+            novaTarefa.appendChild(paragrafo)
+            novaTarefa.appendChild(quebra_linha)
+
+            verificar1.value = ''
+        }
+    }
+}
