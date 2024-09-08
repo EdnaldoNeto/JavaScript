0f4e84c96717893912a77438f6113b7fed34327d
Ednaldo_Neto <ednaldoramosneto@gmail.com>
2024-09-08 01:47:13 -0300

    estrutura de repetição

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,13 @@
+// Estruras de repetição
+
+var i = 1 //contador
+
+do {
+    console.log(`O contador está com o valor de ${i}`)
+    i++
+} while(i <= 5)
+
+/*while (i <= 5) {
+    console.log(`O contador está com o valor de ${i}`)
+    i++ // i = i + 1
+}*/
