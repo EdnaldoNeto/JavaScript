73315c6c0e1fd3c1890edfe0dae0ae6b99a3879a
Ednaldo_Neto <ednaldoramosneto@gmail.com>
2024-09-12 20:00:16 -0300

    update tb

--- aulas/ex04/tb_script.js
+++ aulas/ex04/tb_script.js
@@ -1,32 +1,19 @@
-var n = window.document.getElementById('num')
-var table = window.document.getElementById('res')
-var but1 = document.getElementById('bt1')
-var but2 = document.getElementById('bt2')
-but2.disabled = true
-
 function tab() 
 {
-        var i = 1
+    var n = window.document.getElementById('num')
+    var table = window.document.getElementById('res')
+    var i = 1
     var numero = n.value
-    var but1 = document.getElementById('bt1')
     if(numero.length <= 0)
     {
         alert('Digite um número')
     } else {
+        table.innerHTML = 'Tabuada: <br>'
         while(i <= 10)
-        {
+        {   
             var multi = (numero) * (i)
             table.innerHTML += `${numero} x ${i} = ${multi}<br>`
             i++
         }
-        but1.disabled = true
-        but2.disabled = false
     }
-}
-
-function clean()
-{
-    table.innerHTML = ''
-    but1.disabled = false
-    but2.disabled = true
 }
