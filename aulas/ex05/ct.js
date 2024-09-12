357f890f0ac7853aab58809431ee844756e273b2
Ednaldo_Neto <ednaldoramosneto@gmail.com>
2024-09-12 00:34:34 -0300

    update contador

--- aulas/ex05/ct.js
+++ aulas/ex05/ct.js
@@ -1,6 +1,22 @@
-var inicio = document.getElementById('ini')
+var ini = document.getElementById('ini')
+var fi = document.getElementById('fim')
+var resultado = document.getElementById('res')
+
+
 
 function contar()
 {
+    var inicio = Number(ini.value)
+    var fim = Number(fi.value)
+    var emoji =  String.fromCodePoint(0x1F974)
 
+    if(inicio > fim){
+        alert('digite números válidos')
+    } else {
+        while (inicio <= fim)
+            {
+                resultado.innerHTML = `${inicio}${emoji}<br>`
+                inicio++
+            }
+    }
 }
