5a7ffe279fa5099f0ed20783e701189b8617bf36
Ednaldo_Neto <ednaldoramosneto@gmail.com>
2024-09-12 17:59:12 -0300

    Update ct.js

--- aulas/ex05/ct.js
+++ aulas/ex05/ct.js
@@ -1,22 +1,32 @@
-var ini = document.getElementById('ini')
-var fi = document.getElementById('fim')
-var resultado = document.getElementById('res')
-
-
-
 function contar()
 {
-    var inicio = Number(ini.value)
-    var fim = Number(fi.value)
-    var emoji =  String.fromCodePoint(0x1F974)
+    var ini = document.getElementById('ini')
+    var fi = document.getElementById('fim')
+    var pss = document.getElementById('pss')
+    var resultado = document.getElementById('res')
 
-    if(inicio > fim){
-        alert('digite números válidos')
+    if(ini.value.length == 0 || fi.value.length == 0 || pss.value.lenght == 0) {
+        alert('Faltam dados')
     } else {
-        while (inicio <= fim)
-            {
-                resultado.innerHTML = `${inicio}${emoji}<br>`
-                inicio++
+        resultado.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fi.value)
+        let p = Number(pss.value)
+        if (p == 0)
+        {
+            alert('Passo inválido! Considerando passo 1')
+            p = 1
+        }
+        if (i < f)
+        {
+            for (let c = i; c <= f; c += p) {
+                resultado.innerHTML += ` ${c} \u{1f449}`
             }
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                resultado.innerHTML += ` ${c} \u{1f449}`
+            }
+        }
+        resultado.innerHTML += `\u{1f3c1}`
     }
 }
