52a3cb49350c250cb7079b4aba11caeb4e3df010
Ednaldo_Neto <ednaldoramosneto@gmail.com>
2024-09-16 00:57:21 -0300

    update analisador

--- aulas/ex06/analisador_n.js
+++ aulas/ex06/analisador_n.js
@@ -1,24 +1,30 @@
 var numeros = []
-
+let resultado = document.getElementById('res2')
 function adicionar (){
     let item = document.createElement('option')
     let select = document.getElementById('tab')
     let n = document.getElementById('txtn')
-
-    if (n.value.length == 0){
+    let num = Number(n.value)
+    resultado.innerHTML = ''
+    if (num == 0){
         alert('Digite um número')
     }
-    if(n.value < 0 || n.value > 100){
+    if(num > 100){
         alert('Digite um número válido')
     }
-    if(n.value.length > 0 && n.value <= 100){
-        numeros.push(n.value)
-        item.text = `Número adicionado ${numeros[0]}`
+    if(numeros.includes(num) == true){
+        alert('Esse número ja foi cadastrado, tente outro')
+    }
+    if(num > 0 && num <= 100 && numeros.includes(num) == false){
+        numeros.push(num) // Adicionando elemento no array
+        item.text = `Número adicionado ${num}`
         select.append(item)
+        numeros.sort(function (a, b) { return a - b}) // Faz com que o array seja ordenado de forma crescente
     }
 }
-
 function analisar(){
-    let resultado = document.getElementById('res2')
-    resultado.innerHTML = `A o todo temos ${numeros.length} numeros cadastrados`
+    //let ultimoElemento = numeros[-1]
+    resultado.innerHTML += `A o todo temos ${numeros.length} numeros cadastrados<br>`
+    resultado.innerHTML += `O maior valor informado foi ${numeros[numeros.length - 1]}<br>` // obtendo o valor do ultimo elemento do array
+    resultado.innerHTML += `O menor valor informado foi ${numeros[0]}<br>`
 }
